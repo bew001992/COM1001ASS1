03b4868e7951c99e22bbb67343aa76a74ba2cb5d
Benjamin Williams <bewilliams1@sheffield.ac.uk>
2016-11-28 17:09:58 +0000

    implemented main menu, started options menu

--- floodit.rb
+++ floodit.rb
@@ -1,3 +1,6 @@
+require 'console_splash'
+collumns = 14
+rows = 9
 
 def get_board(width, height)
   # TODO: Implement this method
@@ -20,3 +23,45 @@ end
 
 # TODO: Implement everything else as described in the
 #       assignment brief.
+def splashScreen(height,width)
+  splash = ConsoleSplash.new(height,width)
+  splash.write_header("Splash It", "Ben Williams", "0.1.0")
+  splash.write_horizontal_pattern("v^")
+  splash.write_vertical_pattern("|")
+  splash.splash
+  gets
+  `clear`
+end
+
+def mainMenu(q)
+  quit = q
+  highScore = 0
+  begin
+  puts "Main menu: "
+  puts "S = Start"
+  puts "C = Change size"
+  puts "Q = Quit"
+  if highScore == 0 then
+    puts "No games played yet"
+    else
+    puts "Highscore: #{highScore}"
+  end
+    print "Please enter your choice: "
+    input = gets.chomp.upcase
+    if input == 'S' then 
+      mainGame()
+      elsif input == 'C'
+      changeSize()
+      elsif input == 'Q' then 
+          quit = true 
+      end
+      return quit
+      print quit 
+    end until quit == true
+ end
+     
+def changeSize(collumns, rows)
+ 
+end
+      splashScreen(20,30)
+     mainMenu(false)
